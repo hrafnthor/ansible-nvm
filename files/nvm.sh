286996961055d365a942f303532b228c9affeee7
Hrafn Thorvaldsson <hrafn@hth.is>
2024-01-14 20:23:12 +0000

    nvm version download

---
+++ files/nvm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+# Environment configuration for NVM
+
+export NVM_DIR=""
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
